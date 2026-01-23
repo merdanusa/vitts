71796a03748da67125a17ab08284d91c1aa2fe9a
Merdan <wisew.tube@gmail.com>
2026-01-24 00:09:31 +0500

    init commit

--- app/auth/_layout.tsx
+++ app/auth/_layout.tsx
@@ -1,16 +1,15 @@
-
 import { RootState } from "@/store";
 import { Redirect, Stack } from "expo-router";
 import { useSelector } from "react-redux";
 
 export default function AuthLayout() {
   const isAuthenticated = useSelector(
-    (state: RootState) => state.user.isAuthenticated
+    (state: RootState) => state.user.isAuthenticated,
   );
 
   if (isAuthenticated) {
     return <Redirect href="/(tabs)" />;
   }
 
-  return <Stack />;
+  return <Stack screenOptions={{ headerShown: false }} />;
 }
